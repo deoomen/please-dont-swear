11b4a0b5ce40e0e2783a141935e58ca764801a93
deoomen <deoomen@protonmail.com>
2020-07-09 20:00:49 +0200

    censor method, add docblocks

--- src/PleaseDontSwear.php
+++ src/PleaseDontSwear.php
@@ -1,18 +1,60 @@
 <?php
+/**
+ * PleaseDontSwear package main file
+ * PHP Version: 7.4
+ *
+ * @category Description
+ * @package  PleaseDontSwear
+ * @author   deoomen <deoomen@protonmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/deoomen/please-dont-swear
+ */
 
 namespace PleaseDontSwear;
 
+/**
+ * PleaseDontSwear main class
+ */
 class PleaseDontSwear
 {
-    private array $_curses;
+    private array $_swears;
 
+    /**
+     * Initially loads dictionary
+     */
     public function __construct()
     {
-        $this->_curses = \json_decode(\file_get_contents(__DIR__ . '/curses-pl.json'));
+        $this->_swears = \json_decode(
+            \file_get_contents(__DIR__ . '/swears-pl.json')
+        );
     }
 
+    /**
+     * Censor the text by fully masking swears
+     *
+     * @param string $textToCensor Text you want to be censored
+     *
+     * @return string
+     */
     public function censor(string $textToCensor): string
     {
-        return 'PleaseDontSwear->test';
+        $censoredText = $textToCensor;
+        foreach ($this->_swears as $swear) {
+            $position = \strpos($censoredText, $swear);
+            if ($position !== false) {
+                $censor = "";
+                $swearLength = \strlen($swear);
+                for ($i = 0; $i < $swearLength; $i++) {
+                    $censor .= "*";
+                }
+                $censoredText = \preg_replace(
+                    "/{$swear}\b/i",
+                    $censor,
+                    $censoredText
+                );
+            }
+        }
+
+        return $censoredText;
     }
 }
