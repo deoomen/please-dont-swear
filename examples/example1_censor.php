8f3f65cace711fce26c5b2901481e7a58fe27ba4
deoomen <deoomen@protonmail.com>
2020-07-13 20:14:44 +0200

    small refactor

--- examples/example1_censor.php
+++ examples/example1_censor.php
@@ -7,11 +7,13 @@ require_once __DIR__ . '/../src/PleaseDontSwear.php';  // unnecesary if you use 
 
 use PleaseDontSwear\PleaseDontSwear;
 
+$PDS = new PleaseDontSwear();
+
 // Aleksander Fredro, "Sztuka obłapiania" (original title "Sztuka jebania"), 1817
 $myBadText = "Jeb, Młodzieńcze, jeb, lecz miej zwyczaj drogi,
 Ze świecą kurwie patrzeć między nogi.";
 
-$myCensoredText = (new PleaseDontSwear())->censor($myBadText);
+$myCensoredText = $PDS->censor($myBadText);
 
 echo $myCensoredText;
 // Jeb, Młodzieńcze, jeb, lecz miej zwyczaj drogi,
